7df1db6b3629d6bb24704f87e55ca5adad1ae8e4
Monzae <Alexander.Hedvall@Hotmail.com>
2015-04-27 16:03:05 +0200

    Menu "Skeleton" for the game

---
+++ Infected Zoo Menu/Assets/Scripts/ChangeCharacter.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeCharacter : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
