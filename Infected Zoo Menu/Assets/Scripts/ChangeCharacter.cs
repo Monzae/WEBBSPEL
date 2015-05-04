04d20977d5bb828955aef91f3c5a4fc59e631978
Monzae <Alexander.Hedvall@Hotmail.com>
2015-05-04 11:36:53 +0200

    Updated menu

--- Infected Zoo Menu/Assets/Scripts/ChangeCharacter.cs
+++ Infected Zoo Menu/Assets/Scripts/ChangeCharacter.cs
@@ -1,15 +1,50 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class ChangeCharacter : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+    public Image image;
+    public List<Sprite> imageList;
+    private Sprite activeImage;
+    private int portNumber;
+	void Start () 
+    {
+        portNumber = 0;
 	}
 	
-	// Update is called once per frame
-	void Update () {
+	void Update () 
+    {
 	
 	}
+    public void ChangeCharacterPortraitForward()
+    {
+        if (portNumber < (imageList.Count - 1))
+        {
+            this.activeImage = (Sprite)Instantiate(imageList[portNumber += 1]);
+            image.sprite = activeImage;
+        }
+        else
+        { 
+            portNumber = 0;
+            this.activeImage = (Sprite)Instantiate(imageList[portNumber]);
+            image.sprite = activeImage;
+        }
+    }
+
+    public void ChangeCharacterPortraitBackward()
+    {
+        if (portNumber > (0))
+        {
+            this.activeImage = (Sprite)Instantiate(imageList[portNumber -= 1]);
+            image.sprite = activeImage;
+        }
+        else
+        {
+            portNumber = (imageList.Count-1);
+            this.activeImage = (Sprite)Instantiate(imageList[portNumber]);
+            image.sprite = activeImage;
+        }
+    }
 }
---
+++ Infected Zoo Menu/Assets/Scripts/PlayerName.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerName : MonoBehaviour {
+    public string player1Name;
+    public string player2Name;
+	void Start () 
+    {
+        player1Name = "Player 1";
+        player2Name = "Player 2";
+	}
+	
+	void Update () 
+    {
+	
+	}
+    public void Player1NameChange(string value)
+    {
+        if (player1Name == "")
+        {
+            player1Name = "Player 1";
+        }
+        else
+        player1Name = value;
+    }
+
+    public void Player2NameChange(string value)
+    {
+        if (player2Name == "")
+        {
+            player2Name = "Player 2";
+        }
+        else
+        player2Name = value;
+    }
+}
