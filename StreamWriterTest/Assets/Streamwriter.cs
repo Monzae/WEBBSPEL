c364a936cb2425bc11fc6d61fceaa1bf600c2a5c
Monzae <Alexander.Hedvall@Hotmail.com>
2015-05-06 10:22:21 +0200

    Random commit

---
+++ StreamWriterTest/Assets/Streamwriter.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using UnityEngine.UI;
+
+public class Streamwriter : MonoBehaviour {
+
+    public Text score;
+    public Text name;
+    public Text submitButton;
+    private bool ScoreSubmitted;
+	void Start () 
+    {
+        ScoreSubmitted = false;
+	}
+	
+	void Update () 
+    {
+	
+	}
+
+    public void AddScore()
+    {
+
+        if (!ScoreSubmitted)
+        {
+            using (StreamWriter sw1 = new StreamWriter("Score.txt", true))
+            {
+                if (name.text != "")
+                {
+                    sw1.WriteLine(score.text + "," + name.text.ToString());
+                }
+                else
+                {
+                    sw1.WriteLine(score.text + "," + "Anonymous");
+                }
+                ScoreSubmitted = true;
+                submitButton.text = "Submitted!";
+            } 
+        }
+
+    }
+}
